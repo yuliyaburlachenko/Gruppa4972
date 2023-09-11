f640455433890a60bef01726145a99c7a673ab1a
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-09-12 01:40:34 +0300

    solution from sem2 task9,11,12,14,16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 11
+//Напишите программу,которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+int ran = new System.Random().Next(100, 1000); 
+
+System.Console.WriteLine(ran); 
+
+char[] digitChar = ran.ToString().ToCharArray();
+
+ System.Console.Write(digitChar[0]); 
+
+ System.Console.Write(digitChar[2]);
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 16
+//Напишите прграмму,которая принимает на вход два числаи проверяет,является ли одно число квадратом другого.
+
+int GetNum(){ Console.Write("Введите число: "); 
+return Convert.ToInt32(Console.ReadLine());
+
+ } int firstNum = GetNum(); 
+
+ int secondNum = GetNum();
+
+  bool firstPow = firstNum == Math.Pow(secondNum, 2);
+  
+   bool secondPow = secondNum == Math.Pow(firstNum, 2); 
+
+Console.WriteLine(firstPow || secondPow ? "Одно из чисел является квадратом второго" : "Ни одно из чисел не является квадратом другого");
