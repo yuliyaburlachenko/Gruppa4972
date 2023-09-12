3cfc435720a58ac9b452678f2620efea3ed722b9
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-09-12 21:31:07 +0300

    solution from sem2 task 9.15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13
+//Напишите программу,которая выводит третью цифру заданного числа или ссобщает,что третьей цифры нет.
+
+int number = ReadLine("Введите число: ");
+
+int count = number.ToString().Length;
+
+Console.Write(MakeArray(number, count));
+
+
