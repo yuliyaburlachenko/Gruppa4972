a087f4b1adc665a56d5114e03896bc409d9a8a5c
yuliyaburlachenko <143260884+yuliyaburlachenko@users.noreply.github.com>
2023-09-12 20:19:54 +0300

    Update Program.cs

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -36,19 +36,7 @@ void Variant2()
     Console.WriteLine(number1);
 
     Console.WriteLine((number1 / 10 > number1 % 10) ? (int)(number1 / 10) : (int)(number1 % 10));
-}
 
-void Variant3()
-{
-    Console.WriteLine("Способ 3");
-
-    System.Random numberGenerator = new System.Random();
-    int number = numberGenerator.Next(10, 100);
-    Console.WriteLine(number);
-
-    char[] digitChar = number.ToString().ToCharArray();
-    Console.WriteLine((int)digitChar[0]>(int)digitChar[1]?digitChar[0]:digitChar[1]);
-    //<условие>?<вариант1>:<вариант2>
 
 }
 
