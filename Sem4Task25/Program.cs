e9b8f6da76633c3848599c18dd462463bde763fd
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-09-17 16:25:31 +0300

    solution from sem 4 task 25,27,29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+//натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("введите число A");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число B");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+
+step = step * a;
+}
+
+Console.WriteLine("A в степени B равно: " + step);
+
+
+
