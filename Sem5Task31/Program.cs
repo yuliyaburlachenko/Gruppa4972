c3f6ae35a2c281bfdf0ddae77f7332c4ab76d6db
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-09-21 20:42:44 +0300

    solution from sem5 task31,32,33,35

---
+++ Sem5Task31/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задача 31
+//Задайте массив из 12 элементов, заполненный 
+//случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных 
+//элементов массива.
+//Например: 
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+//сумма положительных чисел равна 29, сумма 
+//отрицательных равна -20.
+
+﻿//Заполняем одномерный массив
+
+
+int[] GenArray(int num, int lowBorder, int highBorder){
+    
+    int[] array = new int[num];
+    
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder+1);
+    }
+
+    return array;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+(int, int) NegotivPositivSum(int[] arr)
+{
+    int posSum = 0;
+    int negSum = 0;
+
+    for(int i =0;i<arr.Length;i++)
+    {
+        if(arr[i]>0)
+        {
+            posSum=posSum+arr[i];
+        }
+        else
+        {
+            negSum+=arr[i];
+        }
+    }
+    return (posSum,negSum);
+}
+
+int[] array = GenArray(12,-9,9);
+PrintArray(array);
+(int pos, int neg) sum = NegotivPositivSum(array);
+
+Console.WriteLine("Сумма >0 :"+sum.pos+" Сумма <0 :"+sum.neg);
+
---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача №32 
+//Напишите программу замена элементов массива: 
+//положительные элементы замените на 
+//соответствующие отрицательные, и наоборот.
+
+
+Random rnd = new Random();
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+int[] create1DArray(int len, int num1, int num2)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(Math.Min(num1, num2), Math.Max(num1, num2));
+    }
+    return arr;
+}
+void show1DArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "\t");
+    }
+    Console.WriteLine(" ");
+}
+int[] reverse1DArray(int[] array)
+{
+    int[] arr=array;
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i]*=-1;
+    }
+    return arr;
+}
+int len = InputNum("Введите длинну массива: ");
+int num1 = InputNum("Введите минимум для значений в массиве: ");
+int num2 = InputNum("Введите максимум для значений в массиве: ");
+Console.WriteLine("Оригинал:");
+int[]arr=create1DArray(len,num1,num2);
+show1DArray(arr);
+Console.WriteLine("реверс:");
+int[]reverse_arr=reverse1DArray(arr);
+show1DArray(arr);
+    
