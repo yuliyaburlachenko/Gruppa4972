ad9d0c5ae1d2630d20e6bd4906f5e2be2a3d47fe
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-09-24 02:20:17 +0300

    solution from sem 6 task 39,40,42,44,45

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №40 
+//Напишите программу, которая принимает на вход 
+//три числа и проверяет, может ли существовать 
+//треугольник с сторонами такой длины.
+
+// Вывод сообщения и запись введённых данных
+int[] Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    return StringToIntArray(value);
+}
+
+// Преобразовывает строку в массив чисел
+int[] StringToIntArray(string value)
+{
+    string[] values = value.Split(",");
+    int[] triangleSides = new int[values.Length];
+    for (int i = 0; i < values.Length; i++)
+        triangleSides[i] = Convert.ToInt32(values[i]);
+    
+    return triangleSides;
+}
+
+// Проверяет, может ли существовать треугольник
+bool CheckTriangle(int[] triangleSides)
+{
+    return (triangleSides[0] < triangleSides[1] + triangleSides[2]) 
+            && (triangleSides[1] < triangleSides[0] + triangleSides[2]) 
+            && (triangleSides[2] < triangleSides[0] + triangleSides[1]) ;
+}
+
+int[] triangleSides = Prompt("Введите стороны треугольника через запятую: ");
+
+Console.Write(CheckTriangle(triangleSides) ? "Треугольник со сторонами такой длины МОЖЕТ существовать." : "Треугольник со сторонами такой длины НЕ МОЖЕТ существовать.");
