66bad7d0de648e34d1bc3aab01ad9eafd5ef5dfd
Yuliya Burlachenko <yuliya4015@yandex.ru>
2023-10-01 13:44:25 +0300

    solution from sem8 task 53,54,55,56,57,58,59,60,61,62

---
+++ Sem8Task55/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача №55 
+//Задайте двумерный массив. Напишите программу, 
+//которая заменяет строки на столбцы. В случае, 
+//если это невозможно, программа должна вывести 
+//сообщение для пользователя.
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = new Random().Next(downBorder, topBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void TransponationMatrix(int[,] matrix)
+{
+    if(matrix.GetLength(0) == matrix.GetLength(1)){
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i+1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[j,i];
+            matrix[j,i] = matrix[i,j];
+            matrix[i,j] = temp;
+        }
+
+    }
+    } else {
+        Console.WriteLine("Эту матрицу нельзя перевернуть");
+    }
+}
+
+Console.Clear();
+int n = ReadData("Количество строк: ");
+int m = ReadData("Количество столбцов: ");
+Console.WriteLine("Исходная матрица");
+int[,] array2D = Fill2DArray(n,m,10,1);
+Print2DArray(array2D);
+Console.WriteLine("________________");
+TransponationMatrix(array2D);
+Print2DArray(array2D);
---
+++ Sem8Task59/Program.cs
@@ -0,0 +1,112 @@
+﻿// Задача №59
+//Задайте двумерный массив из целых чисел. 
+//Напишите программу, которая удалит строку и 
+//столбец, на пересечении которых расположен 
+//наименьший элемент массива.
+
+// Ввод парметров матрицы.
+int countRow = ReadInt("Введите количество строк m: ");
+int countCol = ReadInt("Введите количество cтолбцов n: ");
+
+// Генерация и вывод матрицы.
+int minValue = -10;
+int maxValue = 10;
+int[,] matrix = CreateRandomMatrix(countRow, countCol, minValue, maxValue);
+PrintMatrix("Заданная матрица", matrix);
+
+// Поиск индексов минимального элемента
+(int smallestValue, int colIndex, int rowIndex) min = FindMinElement(matrix);
+Console.WriteLine ($"Наименьший элемент: {min.smallestValue} позиция [{min.rowIndex},{min.colIndex}]");
+
+// Удаление строки и столбца на пересечении элемента, который наименьший в массиве и вывод нового массива
+int[,] resultMatrix = RemoveRowAndColumn(matrix, min.rowIndex, min.colIndex);
+PrintMatrix("Измененная матрица", resultMatrix);
+
+
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генераци двумерного массива, заполненного случайными целыми значениями.
+int[,] CreateRandomMatrix(int countRow, int countCol, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[,] matrix = new int[countRow, countCol];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countCol; j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+// Вывод двумерного массива на экран.
+void PrintMatrix(string msg, int[,] matrix)
+{
+    Console.WriteLine("\n" + msg);
+
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Поиск наименьшего элемента в массиве и его индекса
+(int smallestValue,  int colIndex, int rowIndex) FindMinElement(int[,] matrix)
+{
+    int smallestValue = int.MaxValue;
+    int rowIndex = -1;
+    int colIndex = -1;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < smallestValue)
+            {
+                smallestValue = matrix[i, j];
+                rowIndex = i;
+                colIndex = j;
+            }
+        }
+    }
+    return (smallestValue, rowIndex, colIndex);
+}
+
+// Удаление строки и столбца из двумерного массива
+int[,] RemoveRowAndColumn(int[,] matrix, int rowIndex, int colIndex)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    int[,] resultMatrix = new int[rowCount - 1, colCount - 1];
+
+    for (int i = 0, newRow = 0; i < rowCount; i++)
+    {
+        if (i == rowIndex) continue; // Если индекс строки равен индексу минимального, его пропускаем
+
+        for (int j = 0, newCol = 0; j < colCount; j++)
+        {
+            if (j == colIndex) continue; // Если индекс столбца равен индексу минимального, его пропускаем
+
+            resultMatrix[newRow, newCol] = matrix[i, j];
+            newCol++;
+        }
+        newRow++;
+    }
+
+    return resultMatrix;
+}
